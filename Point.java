ec123fd81944a2fb06911d0990059383ae690882
Artem <56403067+ArtemSerikov@users.noreply.github.com>
2019-12-09 20:18:23 +0300

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,57 @@
+package ru.avalon.java.j20.labs.models;
+
+/**
+ * Модель представления о точке.
+ */
+public class Point <T extends Number> {
+    /**
+     * Абсцисса точки.
+     */
+    private final T x;
+    /**
+     * Ордината точки.
+     */
+    private final T y;
+
+    /**
+     * Основной конструктор класса.
+     *
+     * @param x абсцисса точки
+     * @param y ордината точки
+     */
+    public Point(T x, T y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Возвращает абсциссу точки.
+     *
+     * @return x-координата точки.
+     */
+    public T getX() {
+        return x;
+    }
+
+    /**
+     * Возвращает ординату точки.
+     *
+     * @return y-координата точки.
+     */
+    public T getY() {
+        return y;
+    }
+
+    /**
+     * Возвращает дистанцию от точки до точки.
+     *
+     * @param point точка, до которой следует вычислить
+     *              дистанцию.
+     * @return дистанция между точками
+     */
+    public <T extends Point> double distanceTo(Point point) {
+        double dx = x.doubleValue() - point.x.doubleValue();
+        double dy = y.doubleValue() - point.y.doubleValue();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
